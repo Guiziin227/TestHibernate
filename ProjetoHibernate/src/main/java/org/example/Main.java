f4e559df62e9574607e74e4555348f5158a5098b
Guiziin227 <gh63388@gmail.com>
2025-04-22 21:11:00 -0300

    feat: implement JPA persistence for Pessoa objects in Main class

--- ProjetoHibernate/src/main/java/org/example/Main.java
+++ ProjetoHibernate/src/main/java/org/example/Main.java
@@ -2,16 +2,26 @@ package org.example;
 
 import dominio.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
 
-        Pessoa p1 = new Pessoa(1, "Guilherme", "gh@gmail.com");
-        Pessoa p2 = new Pessoa(2, "Biju", "biju@gmail.com");
-        Pessoa p3 = new Pessoa(3, "Malu", "balu@gmail.com");
+        Pessoa p1 = new Pessoa(null, "Guilherme", "gh@gmail.com");
+        Pessoa p2 = new Pessoa(null, "Biju", "biju@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Malu", "balu@gmail.com");
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("poggers-jpa"); // Instancia do EntityManagerFactory
+        EntityManager em = emf.createEntityManager(); // Instancia do EntityManager e conexao com o banco de dados
+
+        em.getTransaction().begin();
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit();
+        System.out.println("Persistido com sucesso!");
 
     }
 }
